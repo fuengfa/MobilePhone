9c8f159ff1212919b4b6d47207b2bffafb35cca8
fuengfa <fuengfa1996@gmail.com>
2019-08-27 16:27:48 +0700

    add favorit layout

--- app/src/main/java/com/scb/Mobilephone/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/scb/Mobilephone/ui/main/SectionsPagerAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.scb.Mobilephone.MobileListFragment
 import com.scb.Mobilephone.R
 
 private val TAB_TITLES = arrayOf(
@@ -18,9 +19,10 @@ private val TAB_TITLES = arrayOf(
 class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+        when(position){
+            0 -> return MobileListFragment()
+            else -> return PlaceholderFragment()
+        }
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
